962eb7b470c4bff67a22eef00d841972301c37bd
Barbara Frosik <bfrosik@anl.gov>
2016-05-25 13:14:23 -0500

    test

--- test/test_file.py
+++ test/test_file.py
@@ -73,123 +73,111 @@ def test_conf_error_no_schema():
     open(logfile, 'w').close()
 
 
-@with_setup(init, clean)
-def test_no_schema():
-    conf_file = "dqconfig.ini"
-    find = 'tags'
-    replace = 'tagsx'
-    mod.replace_text_in_file(conf_file, find, replace)
-    # the file.verify will exit with -1
-    try:
-        file.verify(conf_file, None)
-    except:
-        pass
-    assert res.is_text_in_file(logfile, 'configuration error: file schemas/tagsx.json does not exist')
+# @with_setup(init, clean)
+# def test_no_schema():
+#     conf_file = "dqconfig.ini"
+#     find = 'tags'
+#     replace = 'tagsx'
+#     mod.replace_text_in_file(conf_file, find, replace)
+#     # the file.verify will exit with -1
+#     try:
+#         file.verify(conf_file, None)
+#     except:
+#         pass
+#     assert res.is_text_in_file(logfile, 'configuration error: file schemas/tagsx.json does not exist')
 
-@with_setup(init, clean)
-def test_conf_error_no_type():
-    conf_file = "dqconfig.ini"
-    find = 'verification_type'
-    replace = 'verification_typex'
-    mod.replace_text_in_file(conf_file, find, replace)
-    # the file.verify will exit with -1
-    try:
-        file.verify(conf_file, None)
-    except:
-        pass
-    assert res.is_text_in_file(logfile, 'config error: verification type not configured')
-
-
-@with_setup(init, clean)
-def test_bad_type():
-    conf_file = "dqconfig.ini"
-    find = 'hdf_structure'
-    replace = 'hdf_structurex'
-    mod.replace_text_in_file(conf_file, find, replace)
-    # the file.verify will exit with -1
-    try:
-        file.verify(conf_file, None)
-    except:
-        pass
-    assert res.is_text_in_file(logfile, 'configured verification type hdf_structurex is not supported')
+# @with_setup(init, clean)
+# def test_conf_error_no_type():
+#     conf_file = "dqconfig.ini"
+#     find = 'verification_type'
+#     replace = 'verification_typex'
+#     mod.replace_text_in_file(conf_file, find, replace)
+#     # the file.verify will exit with -1
+#     try:
+#         file.verify(conf_file, None)
+#     except:
+#         pass
+#     assert res.is_text_in_file(logfile, 'config error: verification type not configured')
 
 
-@with_setup(init, clean)
-def test_bad_file():
-    conf_file = "dqconfig.ini"
-    data_file = "data/test_datax.h5"
-    # the file.verify will exit with -1
-    try:
-        file.verify(conf_file, data_file)
-    except:
-        pass
-    assert res.is_text_in_file(logfile, 'parameter error: file data/test_datax.h5 does not exist')
+# @with_setup(init, clean)
+# def test_bad_type():
+#     conf_file = "dqconfig.ini"
+#     find = 'hdf_structure'
+#     replace = 'hdf_structurex'
+#     mod.replace_text_in_file(conf_file, find, replace)
+#     # the file.verify will exit with -1
+#     try:
+#         file.verify(conf_file, None)
+#     except:
+#         pass
+#     assert res.is_text_in_file(logfile, 'configured verification type hdf_structurex is not supported')
 
 
-@with_setup(init, clean)
-def test_tags_missing_tags():
-    schema_file = "schemas/tags.json"
-    conf_file = "dqconfig.ini"
-    find = 'hdf_structure'
-    replace = 'hdf_tags'
-    mod.replace_text_in_file(conf_file, find, replace)
-    data_file = "data/test_data.h5"
-    file.verify(conf_file, data_file)
-    assert res.is_text_in_file(logfile, '/exchange/missing')
-    assert res.is_text_in_file(logfile, '/exchange/missing1')
+# @with_setup(init, clean)
+# def test_bad_file():
+#     conf_file = "dqconfig.ini"
+#     data_file = "data/test_datax.h5"
+#     # the file.verify will exit with -1
+#     try:
+#         file.verify(conf_file, data_file)
+#     except:
+#         pass
+#     assert res.is_text_in_file(logfile, 'parameter error: file data/test_datax.h5 does not exist')
 
 
-@with_setup(init, clean)
-def test_tags_no_missing_tags():
-    schema_file = "schemas/tags.json"
-    conf_file = "dqconfig.ini"
-    find = 'hdf_structure'
-    replace = 'hdf_tags'
-    mod.replace_text_in_file(conf_file, find, replace)
-    match = 'missing'
-    mod.delete_line_in_file(schema_file, match)
-    data_file = "data/test_data.h5"
-    file.verify(conf_file, data_file)
-    assert not res.is_text_in_file(logfile, 'not found')
+# @with_setup(init, clean)
+# def test_tags_missing_tags():
+#     schema_file = "schemas/tags.json"
+#     conf_file = "dqconfig.ini"
+#     find = 'hdf_structure'
+#     replace = 'hdf_tags'
+#     mod.replace_text_in_file(conf_file, find, replace)
+#     data_file = "data/test_data.h5"
+#     file.verify(conf_file, data_file)
+#     assert res.is_text_in_file(logfile, '/exchange/missing')
+#     assert res.is_text_in_file(logfile, '/exchange/missing1')
 
 
-@with_setup(init, clean)
-def test_struct_missing_tags_attrib():
-    schema_file = "schemas/tags.json"
-    conf_file = "dqconfig.ini"
-    data_file = "data/test_data.h5"
-    file.verify(conf_file, data_file)
-    assert res.is_text_in_file(logfile, '/exchange/missing')
-    assert res.is_text_in_file(logfile, '/exchange/missing1')
-    assert res.is_text_in_file(logfile, 'should be axes:theta_dark:y:x')
-    assert res.is_text_in_file(logfile, 'should be axes:theta_white:y:x')
-    assert res.is_text_in_file(logfile, 'attributes are missing in tag /exchange/theta')
+# @with_setup(init, clean)
+# def test_tags_no_missing_tags():
+#     schema_file = "schemas/tags.json"
+#     conf_file = "dqconfig.ini"
+#     find = 'hdf_structure'
+#     replace = 'hdf_tags'
+#     mod.replace_text_in_file(conf_file, find, replace)
+#     match = 'missing'
+#     mod.delete_line_in_file(schema_file, match)
+#     data_file = "data/test_data.h5"
+#     file.verify(conf_file, data_file)
+#     assert not res.is_text_in_file(logfile, 'not found')
 
 
-@with_setup(init, clean)
-def test_struct_no_missing():
-    schema_file = "schemas/tags.json"
-    conf_file = "dqconfig.ini"
-    match = 'missing'
-    mod.delete_line_in_file(schema_file, match)
-    match = 'axes'
-    mod.delete_line_in_file(schema_file, match)
-    match = 'degrees'
-    mod.delete_line_in_file(schema_file, match)
-    data_file = "data/test_data.h5"
-    file.verify(conf_file, data_file)
-    assert os.stat(logfile).st_size == 0
+# @with_setup(init, clean)
+# def test_struct_missing_tags_attrib():
+#     schema_file = "schemas/tags.json"
+#     conf_file = "dqconfig.ini"
+#     data_file = "data/test_data.h5"
+#     file.verify(conf_file, data_file)
+#     assert res.is_text_in_file(logfile, '/exchange/missing')
+#     assert res.is_text_in_file(logfile, '/exchange/missing1')
+#     assert res.is_text_in_file(logfile, 'should be axes:theta_dark:y:x')
+#     assert res.is_text_in_file(logfile, 'should be axes:theta_white:y:x')
+#     assert res.is_text_in_file(logfile, 'attributes are missing in tag /exchange/theta')
 
 
-# def run_tests():
-#     test_conf_error_no_schema()
-#     test_no_schema()
-#     test_conf_error_no_type()
-#     test_bad_type()
-#     test_bad_file()
-#     test_tags_missing_tags()
-#     test_tags_no_missing_tags()
-#     test_struct_missing_tags_attrib()
-#     test_struct_no_missing()
+# @with_setup(init, clean)
+# def test_struct_no_missing():
+#     schema_file = "schemas/tags.json"
+#     conf_file = "dqconfig.ini"
+#     match = 'missing'
+#     mod.delete_line_in_file(schema_file, match)
+#     match = 'axes'
+#     mod.delete_line_in_file(schema_file, match)
+#     match = 'degrees'
+#     mod.delete_line_in_file(schema_file, match)
+#     data_file = "data/test_data.h5"
+#     file.verify(conf_file, data_file)
+#     assert os.stat(logfile).st_size == 0
 
-#run_tests()
+
