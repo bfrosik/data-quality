6e39d3dca50369b92c3c6f7684864a829d9a7e9a
Barbara Frosik <bfrosik@anl.gov>
2016-05-26 11:42:16 -0500

    test

--- test/test_file.py
+++ test/test_file.py
@@ -1,45 +1,39 @@
-import threading
 import os
-import time
-#from test.test_utils.set_config import init
-from test.test_utils.cleanup import clean
 import test.test_utils.modify_settings as mod
 import test.test_utils.verify_results as res
-from nose import with_setup
 
 import dquality.file as file
 import shutil
 
 logfile = os.path.join(os.getcwd(),"default.log")
-config = os.path.join(os.getcwd(),"test/dqconfig.ini")
 config_test = os.path.join(os.getcwd(),"test/dqconfig_test.ini")
-schemas = os.path.join(os.getcwd(),"test/schemas")
 schemas_test = os.path.join(os.getcwd(),"test/schemas_test")
-tags = os.path.join(schemas,"tags.json")
 tags_test = os.path.join(schemas_test,"tags.json")
+data_file = os.path.join(os.getcwd(),"test/data/test_data.h5")
+id = 0
+
 
 def print_log():
     f = open(logfile, 'r')
     print (f.read())
-    
+
 def init():
+    id += 1
+    config = 'test/conf' + id
+    config =  os.path.join(os.getcwd(),config)
     shutil.copyfile(config_test, config)
-    if not os.path.isdir(schemas):
-        shutil.copytree(schemas_test, schemas)
-    else:
-        shutil.copyfile(tags_test, tags)
+    tags = 'test/tags' + id
+    tags = os.path.join(os.getcwd(), tags)
+    shutil.copyfile(tags_test, tags)
+    return config, tags
 
 
 def clean():
-    os.remove(tags)
-    os.remove(config)
     open(logfile, 'w').close()
 
 
 def test_conf_error_no_schema():
-    shutil.copyfile(config_test, config)
-    shutil.copytree(schemas_test, schemas)
-
+    config, tags = init()
     find = 'schema'
     replace = 'schemax'
     mod.replace_text_in_file(config, find, replace)
@@ -48,20 +42,13 @@ def test_conf_error_no_schema():
         file.verify(config, None)
     except:
         pass
-    print_log()
     assert res.is_text_in_file(logfile, 'configuration error: schema is not configured')
 
-    os.remove(tags)
-
-    if os.path.isfile(config):
-        os.remove(config)
-
-    open(logfile, 'w').close()
+    clean()
 
 
-#@with_setup(init, clean)
 def test_no_schema():
-    init()
+    config, tags = init()
     find = 'tags'
     replace = 'tagsx'
     mod.replace_text_in_file(config, find, replace)
@@ -70,12 +57,13 @@ def test_no_schema():
         file.verify(config, None)
     except:
         pass
-    print_log()
+
     assert res.is_text_in_file(logfile, 'configuration error: file test/schemas/tagsx.json does not exist')
     clean()
 
-@with_setup(init, clean)
+
 def test_conf_error_no_type():
+    config, tags = init()
     find = 'verification_type'
     replace = 'verification_typex'
     mod.replace_text_in_file(config, find, replace)
@@ -84,12 +72,13 @@ def test_conf_error_no_type():
         file.verify(config, None)
     except:
         pass
-    print_log()
-    assert res.is_text_in_file(logfile, 'config error: verification type not configured')
 
-    
-@with_setup(init, clean)
+    assert res.is_text_in_file(logfile, 'config error: verification type not configured')
+    clean()
+
+
 def test_bad_type():
+    config, tags = init()
     find = 'hdf_structure'
     replace = 'hdf_structurex'
     mod.replace_text_in_file(config, find, replace)
@@ -99,11 +88,11 @@ def test_bad_type():
     except:
         pass
     assert res.is_text_in_file(logfile, 'configured verification type hdf_structurex is not supported')
-    print_log()
+    clean()
 
 
-@with_setup(init, clean)
 def test_bad_file():
+    config, tags = init()
     data_file = "data/test_datax.h5"
     # the file.verify will exit with -1
     try:
@@ -111,62 +100,51 @@ def test_bad_file():
     except:
         pass
     assert res.is_text_in_file(logfile, 'parameter error: file data/test_datax.h5 does not exist')
-    print_log()
+    clean()
 
-
-@with_setup(init, clean)
 def test_tags_missing_tags():
-    schema_file = os.path.join(os.getcwd(),"test/schemas/tags.json")
+    config, tags = init()
     find = 'hdf_structure'
     replace = 'hdf_tags'
     mod.replace_text_in_file(config, find, replace)
-    data_file = os.path.join(os.getcwd(),"test/data/test_data.h5")
     file.verify(config, data_file)
     assert res.is_text_in_file(logfile, '/exchange/missing')
     assert res.is_text_in_file(logfile, '/exchange/missing1')
-    print_log()
+    clean()
 
 
-@with_setup(init, clean)
 def test_tags_no_missing_tags():
-    schema_file = os.path.join(os.getcwd(),"test/schemas/tags.json")
+    config, tags = init()
     find = 'hdf_structure'
     replace = 'hdf_tags'
     mod.replace_text_in_file(config, find, replace)
     match = 'missing'
-    mod.delete_line_in_file(schema_file, match)
-    data_file = os.path.join(os.getcwd(),"test/data/test_data.h5")
+    mod.delete_line_in_file(tags, match)
     file.verify(config, data_file)
     assert not res.is_text_in_file(logfile, 'not found')
-    print_log()
+    clean()
 
 
-@with_setup(init, clean)
 def test_struct_missing_tags_attrib():
-    schema_file = os.path.join(os.getcwd(),"test/schemas/tags.json")
-    data_file = os.path.join(os.getcwd(),"test/data/test_data.h5")
+    config, tags = init()
     file.verify(config, data_file)
     assert res.is_text_in_file(logfile, '/exchange/missing')
     assert res.is_text_in_file(logfile, '/exchange/missing1')
     assert res.is_text_in_file(logfile, 'should be axes:theta_dark:y:x')
     assert res.is_text_in_file(logfile, 'should be axes:theta_white:y:x')
     assert res.is_text_in_file(logfile, 'attributes are missing in tag /exchange/theta')
-    print_log()
+    clean()
 
 
-@with_setup(init, clean)
 def test_struct_no_missing():
-    schema_file = os.path.join(os.getcwd(),"test/schemas/tags.json")
+    config, tags = init()
     match = 'missing'
-    mod.delete_line_in_file(schema_file, match)
+    mod.delete_line_in_file(tags, match)
     match = 'axes'
-    mod.delete_line_in_file(schema_file, match)
+    mod.delete_line_in_file(tags, match)
     match = 'degrees'
-    mod.delete_line_in_file(schema_file, match)
-    data_file = os.path.join(os.getcwd(),"test/data/test_data.h5")
+    mod.delete_line_in_file(tags, match)
     file.verify(config, data_file)
     assert os.stat(logfile).st_size == 0
-    print_log()
+    clean
 
-
-test_no_schema()
