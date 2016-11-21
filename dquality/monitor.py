3d418f192ffdacbc58fd5bb871f1c86f85d60b9e
Barbara Frosik <bfrosik@anl.gov>
2016-11-21 15:19:10 -0600

    Testing all events

--- dquality/monitor.py
+++ dquality/monitor.py
@@ -203,7 +203,7 @@ def directory(directory, patterns):
                         break
 
     wm = WatchManager()
-    mask = pyinotify.IN_CLOSE_WRITE | pyinotify.IN_CLOSE_NOWRITE | pyinotify.IN_CREATE | pyinotify.IN_ATTRIB |pyinotify.IN_MOVED_TO
+    mask = pyinotify.IN_CLOSE_WRITE | pyinotify.IN_ALL_EVENTS
     handler = EventHandler()
     notifier = pyinotify.Notifier(wm, handler, timeout=1)
     wdd = wm.add_watch(directory, mask, rec=False)
