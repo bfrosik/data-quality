fd40c0f055c700a553d18876501bad8918b2490c
Barbara Frosik <bfrosik@anl.gov>
2016-11-21 15:07:06 -0600

    Added IN_CLOSE_NOWRITE event

--- dquality/monitor.py
+++ dquality/monitor.py
@@ -203,7 +203,7 @@ def directory(directory, patterns):
                         break
 
     wm = WatchManager()
-    mask = pyinotify.IN_CLOSE_WRITE | pyinotify.IN_CREATE
+    mask = pyinotify.IN_CLOSE_WRITE | pyinotify.IN_CLOSE_NOWRITE
     handler = EventHandler()
     notifier = pyinotify.Notifier(wm, handler, timeout=1)
     wdd = wm.add_watch(directory, mask, rec=False)
