cc184a3f8510dfe2768904f2198fe79f83603474
bfrosik <bfrosik@anl.gov>
2017-04-11 10:55:11 -0500

    Moved FileSeek class to monitor_polling

--- dquality/monitor_polling.py
+++ dquality/monitor_polling.py
@@ -65,7 +65,8 @@ import json
 import dquality.common.utilities as utils
 import dquality.common.constants as const
 import dquality.data as dataver
-from dquality.common.containers import FileSeek
+from threading import Timer
+import stat
 
 
 __author__ = "Barbara Frosik"
@@ -75,6 +76,252 @@ __all__ = ['init',
            'verify']
 
 INTERRUPT = 'interrupt'
+
+
+class FileSeek():
+    """
+    This class provides file discovery functionality. An instance is initialized with parameters.
+    On the start the FileSeek checks for existing files. After that it periodically checks
+    for new files in monitored directory and subdirectories. Upon finding a new or updated file it
+    velidates the file and enqueues into the queue on success.
+    """
+
+    def __init__(self, q, polling_period, logger, file_type):
+        """
+        constructor
+
+        Parameters
+        ----------
+        q : Queue
+            a queue used to pass discovered files
+
+        polling_period : int
+            number of second defining polling period
+
+        logger : Logger
+            logger instance
+
+        file_type : int
+            a constant defining type of data file. Supporting FILE_TYPE_GE and FILE_TYPE_HDF
+        """
+        self.q = q
+        self.polling_period = polling_period
+        self.processed_files = None
+        self.logger = logger
+        self.file_type = file_type
+        self.done = False
+
+    def get_files(self, folder, ext, discovered_files=None):
+        """
+        Finds files in a given directory and subdirectories.
+
+        Parameters
+        ----------
+        folder : str
+            a folder name that will be checked for files
+
+        ext : list
+            list of file extensions that will be discovered
+
+        discovered_files : dict
+            a dictionary to add the discovered files to
+
+        Returns
+        -------
+        dict
+            a dictionary with keys as files in a monitored folder and subdirectories
+        """
+
+        current_files = discovered_files
+        if current_files is None:
+            current_files = {}
+        if os.path.isdir(folder):
+            files = os.listdir(folder)
+            for f in files:
+                full_path = os.path.join(folder, f)
+                if os.path.isfile(full_path):
+                    match = False
+                    for fext in ext:
+                        if f.endswith(fext):
+                            match = True
+                    if match:
+                        statResult = os.stat(full_path)
+                        file_info = {}
+                        file_info['fileSize'] = statResult[stat.ST_SIZE]
+                        file_info['fileModificationTime'] = statResult[stat.ST_MTIME]
+                        current_files[full_path] = file_info
+                elif os.path.isdir(full_path):
+                    current_files = self.get_files(full_path, ext, current_files)
+        return current_files
+
+    def notify(self, file_name):
+        """
+        This method performs action to notify stakeholders about a file.
+        Currently the notification is done by using queue.
+        This method validates the file first. If file validated successfully, the full file name is enqueued.
+        The file might not validate if has been discovered before it is complete. In such situation, the file will
+        be discovered again when updated.
+
+        Parameters
+        ----------
+        file_name : str
+            a full name of a file
+
+        Returns
+        -------
+        none
+        """
+
+        if self.file_type == const.FILE_TYPE_GE:
+            validator = FileValidatorGe()
+            if validator.is_valid(file_name):
+                self.q.put(file_name)
+        else:
+            # the velidator is not implemented, so for now pass all discovered files
+            self.q.put(file_name)
+
+    def process_files(self, previous_files, current_files):
+        """
+        This method compares new latest discovered files with previously found files. The new files are reported
+        through notify to the stakeholders.
+
+        Parameters
+        ----------
+        previous_files : dict
+            a dictionary of previously found files
+
+        current_files : dict
+            a dictionary of currently found files
+
+        Returns
+        -------
+        none
+        """
+
+        for file_name in current_files.keys():
+            if not file_name in previous_files:
+                # new file, must be updated
+                self.notify(file_name)
+            else:
+                # old file, check timestamp
+                prev_file_info = previous_files.get(file_name)
+                prev_modification_time = prev_file_info.get('fileModificationTime', '')
+                file_info = current_files.get(file_name)
+                modification_time = file_info.get('fileModificationTime')
+                if modification_time != prev_modification_time:
+                    # file has been modified, need to process it
+                    self.notify(file_name)
+
+
+    def poll_file_system(self, folder, ext):
+        """
+        This method initiates for polling the file information off of the file system.
+
+        Parameters
+        ----------
+        folder : str
+            a folder name that will be checked for files
+
+        ext : list
+            list of file extensions that will be discovered
+
+        discovered_files : dict
+            a dictionary to add the discovered files to
+
+        Returns
+        -------
+        none
+        """
+
+        try:
+            current_files = self.get_files(folder, ext)
+            self.process_files(self.processed_files, current_files)
+            self.processed_files = current_files
+        except:
+            self.logger.error('Could not poll directory %s' % (folder))
+        self.start_observing(folder, ext)
+
+    def start_observing(self, folder, ext):
+        """
+        This method startss for polling the file information off of the file system.
+
+        Parameters
+        ----------
+        folder : str
+            a folder name that will be checked for files
+
+        ext : list
+            list of file extensions that will be discovered
+
+        discovered_files : dict
+            a dictionary to add the discovered files to
+
+        Returns
+        -------
+        none
+        """
+
+        if self.processed_files is None:
+            self.processed_files = self.get_files(folder, ext)
+        if self.done:
+            return
+
+        t = Timer(self.polling_period, self.poll_file_system, [folder, ext])
+        self.t = t
+        t.start()
+
+    def stop_observing(self):
+        """
+        This method stops the polling the file information off of the file system.
+
+        Parameters
+        ----------
+        none
+
+        Returns
+        -------
+        none
+        """
+
+        self.t.cancel()
+        self.done = True
+
+
+class FileValidatorGe():
+    """
+    This class is an interface to the concrete file verification functionality.
+    """
+    def is_valid(self, file):
+        """
+        This method checks if the ge file is compatible with the standards.
+
+        Parameters
+        ----------
+        file : str
+            file name
+
+        Returns
+        -------
+        True if validated, False otherwise
+        """
+
+        import struct as st
+
+        fp = open(file, 'rb')
+        offset = 8192
+
+        fp.seek(18)
+        size, nframes = st.unpack('<ih',fp.read(6))
+        if size != 2048:
+            return False
+
+        fsize = os.stat(str(fp).split("'")[1]).st_size
+        nframes_calc = (fsize - offset)/(2*size**2)
+
+        if nframes != nframes_calc:
+            return False
+
+        return True
 
 
 def init(config):
