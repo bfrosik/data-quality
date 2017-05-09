aababa82b12b40cde4ef27b04a3a203fd1dbba83
bfrosik <bfrosik@anl.gov>
2017-05-09 16:47:20 -0500

    added pcaspy test stub

---
+++ test/pcaspy.py
@@ -0,0 +1,11 @@
+# stub for testing
+
+class SimpleServer:
+    def process(self, value):
+        return True
+
+class Driver:
+    def __init__(self, name):
+        self.name = name
+    def write(self, reason, value):
+        return True
